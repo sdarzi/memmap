c07aa152b41133b7b1553846cdbf7343da73137c
sdarzi <saeiddarzi@gmail.com>
2026-05-03 13:14:18 +0100

    step 2: parse memory regions into struct and print fields

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+typedef struct {
+    unsigned long start;
+    unsigned long end;
+    char perms[5];
+    char pathname[256];
+} MemoryRegion;
+
+
 int main(){
     // open file 
     FILE *current_map = fopen ("/proc/self/maps", "r");
@@ -9,8 +17,19 @@ int main(){
     }
     // read and print each line of the opened file
     char line[256];
+    MemoryRegion region;
     while (fgets(line,sizeof(line),current_map) != NULL){
-        printf("%s",line);
+        region.pathname[0] = '\0';
+        sscanf(line, "%lx-%lx %4s %*x %*x:%*x %*d %255s",
+            &region.start,
+            &region.end,
+            region.perms,
+            region.pathname);
+        printf("START: %lx END: %lx PERMS: %s PATH: %s\n",
+                region.start,
+                region.end,
+                region.perms,
+                region.pathname);
     }
     // close file
     fclose(current_map);
