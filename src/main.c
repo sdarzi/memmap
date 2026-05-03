1b80d773bd3a594047d8636122a6ca7666b1ba1f
sdarzi <saeiddarzi@gmail.com>
2026-05-03 13:45:39 +0100

    slight formatting change

--- src/main.c
+++ src/main.c
@@ -7,29 +7,28 @@ typedef struct {
     char pathname[256];
 } MemoryRegion;
 
-
-int main(){
-    // open file 
-    FILE *current_map = fopen ("/proc/self/maps", "r");
-    if (current_map==NULL){
+int main() {
+    // open file
+    FILE *current_map = fopen("/proc/self/maps", "r");
+    if (current_map == NULL) {
         printf("Failed to open file\n");
         return 1;
     }
     // read and print each line of the opened file
     char line[256];
     MemoryRegion region;
-    while (fgets(line,sizeof(line),current_map) != NULL){
+    while (fgets(line, sizeof(line), current_map) != NULL) {
         region.pathname[0] = '\0';
         sscanf(line, "%lx-%lx %4s %*x %*x:%*x %*d %255s",
-            &region.start,
-            &region.end,
-            region.perms,
-            region.pathname);
-        printf("START: %lx END: %lx PERMS: %s PATH: %s\n",
-                region.start,
-                region.end,
+                &region.start,
+                &region.end,
                 region.perms,
-                region.pathname);
+               region.pathname);
+        printf("START: %lx END: %lx PERMS: %s PATH: %s\n",
+               region.start,
+               region.end,
+               region.perms,
+               region.pathname);
     }
     // close file
     fclose(current_map);
