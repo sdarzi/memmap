5c803cea12253e9bd60db94d899be5be2bc856e1
sdarzi <saeiddarzi@gmail.com>
2026-05-04 13:18:31 +0100

    updated comments

--- src/main.c
+++ src/main.c
@@ -3,19 +3,26 @@
 #include "regions.h"
 
 int main(int argc, char *argv[]) {
+
+
     char path[64];
+    
+    // if no second argument, run on curreent pid
     if (argc < 2) {
         snprintf(path, sizeof(path), "/proc/self/maps");
     } else {
         int pid = atoi(argv[1]);
         snprintf(path, sizeof(path), "/proc/%d/maps", pid);
     }
-    // open file
+
+
+    // open file based on path above
     FILE *current_map = fopen(path, "r");
     if (current_map == NULL) {
         printf("Error: could not open %s - check PID exists and you have permissions\n", path);
         return 1;
     }
+
     // read and print each line of the opened file
     char line[256];
     MemoryRegion region;
@@ -33,7 +40,9 @@ int main(int argc, char *argv[]) {
                region_label(classify_region(&region)),
                region.pathname);
     }
+
     // close file
     fclose(current_map);
+
     return 0;
 }
